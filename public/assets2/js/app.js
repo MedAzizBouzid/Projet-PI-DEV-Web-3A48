8d2ca9e413689826cf40d9636ed72fc57589144b
MedAzizBouzid <Mohamedaziz.bouzid@esprit.tn>
2023-02-28 21:06:30 +0100

    bundle alert

---
+++ public/assets2/js/app.js
@@ -0,0 +1,20 @@
+
+import '../../assets2/css/app.scss';
+import Swal from 'sweetalert2';
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
+  
+  Toast.fire({
+    icon: 'success',
+    title: 'Signed in successfully'
+  })
+
